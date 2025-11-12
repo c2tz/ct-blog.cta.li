5de30cb7f8ce20e90be0805adcf2a222c8b56caa
c2tz <95414249+c2tz@users.noreply.github.com>
2025-11-12 18:17:23 +0000

    chore: first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astro-etch.pages.dev',
+    site: 'https://ct-blog.cta.li/',
     integrations: [mdx(), sitemap(), tailwind(), react()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Etch';
-export const SITE_DESCRIPTION = 'Astro Etch is a blog style template written in Astro. It is optimized for simplicity.';
+export const SITE_TITLE = 'ct-blog.cta.li';
+export const SITE_TAB_TITLE = 'ct-blog.cta.li: mon blog-note'
+export const SITE_DESCRIPTION = 'ct-blog est le blog de c2tz, technicien en informatique.';
