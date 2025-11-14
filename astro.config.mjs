0a08d49a34428dbbbc792c4f6a47c999f95a11bb
c2tz <95414249+c2tz@users.noreply.github.com>
2025-11-14 23:32:08 +0000

    feat: add section anchors and refresh footer links
    Closes #1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,28 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
 export default defineConfig({
-    site: 'https://ct-blog.cta.li/',
-    integrations: [mdx(), sitemap(), tailwind(), react()],
+  site: 'https://ct-blog.cta.li/',
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'wrap', // enveloppe le titre dans un <a>
+          properties: {
+            class: 'heading-link',
+          },
+        },
+      ],
+    ],
+  },
 });
