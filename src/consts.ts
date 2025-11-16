ef7768ea0b621284520ddc1b6c9c5e30399f30d7
c2tz <95414249+c2tz@users.noreply.github.com>
2025-11-16 01:50:48 +0000

    refactor: change code for fonts and for tab title
    Closes #2
    Closes #4

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'ct-blog.cta.li';
-export const SITE_TAB_TITLE = 'ct-blog.cta.li: mon blog-note'
+export const SITE_TAB_TITLE = 'ct-blog: mon blog-note';
+export const SITE_TAB_ARTICLE = 'ct-blog:';
 export const SITE_DESCRIPTION = 'ct-blog est le blog de c2tz, technicien en informatique.';
